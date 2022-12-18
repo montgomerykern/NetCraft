85239d57d8171e9a374950afc3b53cf3285a6d4d
Montgomery James Kern <montgomerykern@users.noreply.github.com>
2022-12-18 02:23:43 +0000

    modified some contact form programing mjk

--- components/Contact/ContactInfo.vue
+++ components/Contact/ContactInfo.vue
@@ -24,33 +24,17 @@
             <div class="col-xxl-7 col-xl-6 col-lg-6">
                 <div class="contact-form">
                         <div class="row">
-                            <form @submit.prevent="handleSubmit"><form action="https://formeezy.com/api/v1/forms/639e7707aa4cca0008ca1096/submissions" method="POST" enctype="multipart/form-data">
-
-                            <div class="col-xxl-6 col-xl-6 col-sm-6 mb-20">
-                                <input v-model="form.name" type="text" name="name" placeholder="Enter name" required>
-                            </div>
-                            <div class="col-xxl-6 col-xl-6 col-sm-6 mb-20">
-                                <input v-model="form.email" type="email" name="email" placeholder="Enter email" required>
-                            </div>
-                            <div class="col-xxl-6 col-xl-6 col-sm-6 mb-20">
-                                <input v-model="form.phone" type="phone" name="phone" placeholder="Enter phone" required>
-                            </div>
-                            <div class="col-xxl-6 col-xl-6 col-sm-6 mb-20">
-                                <input v-model="form.subject" type="subject" name="subject" placeholder="Enter subject" required>
-                            </div>
-                            <div class="col-xxl-12 col-xl-12 col-lg-12 mb-20">
-                                <textarea v-model="form.message" placeholder="Enter message" name="message" required></textarea>
-                            </div>
-                            <div class="col-xxl-12 col-xl-12 mb-20">
-                                <button type="Submit" class="theme-btn border-btn">Send +</button>
-                            </div>
-                            </form>
+                         
+                                <form @submit.prevent="handleSubmit">
+    <input v-model="form.name" type="text" name="name" placeholder="Enter name" required>
+    <input v-model="form.email" type="email" name="email" placeholder="Enter email" required>
+    <textarea v-model="form.message" placeholder="Enter message" name="message" required></textarea>
+    <button type="submit">Send</button>
+  </form>
                         </div>
                 </div>
             </div>
         </div>
-    </div>
-</form>
 </section>
 <!-- contact area end -->
 </template>
@@ -65,8 +49,6 @@
         form: {
           name: "",
           email: "",
-          phone: "",
-          subject: "",
           message: "",
         },
       };
@@ -80,7 +62,7 @@
         }
 
         await axios
-          .post("{Formeezy-Endpoint}", formData)
+          .post("{https://formeezy.com/api/v1/forms/639e7707aa4cca0008ca1096/submissions}", formData)
           .then(({ data }) => {
             const { redirect } = data;
             // Redirect used for reCAPTCHA and/or thank you page
